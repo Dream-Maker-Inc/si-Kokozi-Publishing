8ff46d406e26e724a485dbcb885a0b3664b2daec
Janghyun Han <wkdgus1164@naver.com>
2022-02-16 17:08:01 +0900

    Update - 아띠 탭 : `모두` 화면 리스트 구현

--- src/common/Colors.ts
+++ src/common/Colors.ts
@@ -1,3 +1,4 @@
 export const primaryColor = '#109724'
 export const backgroundColor = '#FAEFE3'
 export const placeholderTextColor = '#CEC0AF'
+export const secondaryColor = '#EEE1D2'
--- src/screens/Atti/no-blank/all/All.tsx
+++ src/screens/Atti/no-blank/all/All.tsx
@@ -1,30 +1,73 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from '@emotion/native'
-import {backgroundColor} from '../../../../common/Colors'
-import {ScrollView, Text} from 'react-native'
+import {backgroundColor, secondaryColor} from '../../../../common/Colors'
+import {FlatList, Text} from 'react-native'
 
-const All = () => <PageWrapper>
-	<ScrollView>
-		<Text>asdf</Text>
-	</ScrollView>
-</PageWrapper>
+const All = () => {
+	const [dataSource, setDataSource] = useState([])
+
+	useState(() => {
+		const items = Array.apply(null, Array(60)).map((v, i) => {
+			return {id: i, src: '../../../../../assets/atti/all/thumbnail.png'}
+		})
+		setDataSource(items)
+	}, [])
+
+	return <PageWrapper>
+		<FlatList
+			data={dataSource}
+			numColumns={2}
+			renderItem={() => (
+				<ListItem>
+					<ItemImage
+						resizeMode="center"
+						source={require('../../../../../assets/atti/all/thumbnail.png')}
+					/>
+					<ItemText>
+						<Text>코코지</Text>
+					</ItemText>
+				</ListItem>
+			)}
+		/>
+	</PageWrapper>
+}
 
 const PageWrapper = styled.SafeAreaView`
-  width: 100%;
   flex: 1;
+  padding: 10px;
   background-color: ${backgroundColor};
 `
 
-const BackgroundText = styled.Image`
-  width: 70%;
-  margin: 0 auto;
-  z-index: 1;
+const ListItem = styled.TouchableOpacity`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  margin: 10px;
+
+  border-radius: 20px;
+  background-color: ${secondaryColor};
 `
 
-const BackgroundImage = styled.Image`
+const ItemImage = styled.Image`
+  width: 100px;
+  height: 180px;
+`
+
+const ItemText = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   width: 100%;
-  height: 500px;
-  margin: -180px auto 0;
+  height: 60px;
+
+  font-weight: bold;
+  background-color: white;
+
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
 `
 
 export default All
