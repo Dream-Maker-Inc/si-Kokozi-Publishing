3ae8dbafd39c063fc4e6e7c3a212161ad9e73ede
Janghyun Han <wkdgus1164@naver.com>
2022-02-17 17:26:40 +0900

    Update - 마이페이지

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import RegisterFirst from './src/screens/Atti/blank/magic-atti/sub/RegisterFirst'
 import BottomTabs from './src/screens/BottomTabs'
-import RegisterSecond from './src/screens/Atti/blank/magic-atti/sub/RegisterSecond'
-import RegisterSuccess from './src/screens/Atti/blank/magic-atti/sub/RegisterSuccess'
-import Help from './src/screens/Atti/blank/magic-atti/sub/etc/Help'
-import AlreadyRegistered from './src/screens/Atti/blank/magic-atti/sub/error/AlreadyRegistered'
-import Detail from './src/screens/Atti/no-blank/detail/Detail'
 
 const App = () => <>
-	<Detail/>
+	<BottomTabs/>
 </>
 
 export default App
--- src/screens/mypage/MyPage.tsx
+++ src/screens/mypage/MyPage.tsx
@@ -1,8 +1,112 @@
 import React from 'react'
-import {Text} from 'react-native-paper'
+import {ScrollView, StatusBar, TouchableOpacity} from 'react-native'
+import styled from '@emotion/native'
+import {backgroundColor, secondaryColor, textDarkColor} from '../../common/Colors'
+import {Paragraph, Title} from 'react-native-paper'
+import {settingListData} from './data'
 
-const MyPage = () => <>
-	<Text>마이페이지</Text>
-</>
+const PageWrapper = styled(ScrollView)`
+  flex: 1;
+  background-color: ${backgroundColor};
+`
+
+const ProfileContainer = styled(TouchableOpacity)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  height: 100px;
+  padding: 20px;
+`
+
+const ProfileInformation = styled.View`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+`
+
+const ProfileImage = styled.Image`
+  width: 60px;
+  height: 60px;
+`
+
+const ProfileName = styled(Title)`
+  margin-left: 20px;
+  font-weight: bold;
+  color: ${textDarkColor};
+`
+
+const Arrow = styled.Image`
+  width: 24px;
+  height: 24px;
+`
+
+const Divider = styled.View`
+  width: 100%;
+  height: 6px;
+  background-color: ${secondaryColor};
+`
+
+const ListContainer = styled.View`
+  width: 100%;
+`
+
+const ListTitle = styled(Title)`
+  font-weight: bold;
+  margin: 20px;
+  color: ${textDarkColor};
+`
+
+const ListItemContainer = styled(TouchableOpacity)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  height: 40px;
+  margin: 20px 24px 0;
+
+  border: none;
+  border-bottom-width: 1px;
+  border-color: ${secondaryColor};
+`
+
+const ListItem = styled(Paragraph)`
+  flex-grow: 1;
+  justify-content: center;
+  color: ${textDarkColor};
+  padding-bottom: 20px;
+  height: 100%;
+  font-weight: bold;
+`
+
+const MyPage = () => <PageWrapper>
+
+	<ProfileContainer>
+		<ProfileInformation>
+			<ProfileImage resizeMode="contain" source={require('../../../assets/mypage/profile-image.png')}/>
+			<ProfileName>코코지</ProfileName>
+		</ProfileInformation>
+		<Arrow resizeMode="cover" source={require('../../../assets/mypage/arrow-right.png')}/>
+	</ProfileContainer>
+
+	<Divider/>
+
+	<ListContainer>
+		{settingListData.map(setting =>
+			<>
+				<ListTitle>{setting.title}</ListTitle>
+				{setting.item.map(list => (
+					<ListItemContainer>
+						<ListItem>{list}</ListItem>
+						<Arrow resizeMode="contain" source={require('../../../assets/mypage/arrow-right.png')}/>
+					</ListItemContainer>
+				))}
+				<Divider/>
+			</>
+		)}
+	</ListContainer>
+
+	<StatusBar barStyle="dark-content" backgroundColor={backgroundColor}/>
+</PageWrapper>
 
 export default MyPage
