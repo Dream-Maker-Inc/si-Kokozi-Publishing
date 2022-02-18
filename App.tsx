cfa8dd1ba9b7fbb4dd34d6299c4ec19f38e01d40
Janghyun Han <wkdgus1164@naver.com>
2022-02-18 15:20:40 +0900

    Update - README.MD

--- App.tsx
+++ App.tsx
@@ -4,9 +4,13 @@ import Playlist from './src/screens/Atti/no-blank/playlist/Playlist'
 import Detail from './src/screens/Atti/no-blank/detail/Detail'
 import PlaylistChange from './src/screens/Atti/no-blank/playlist/PlaylistChange'
 import PlaylistEmpty from './src/screens/Atti/no-blank/playlist/PlaylistEmpty'
+import RegisterFirst from './src/screens/Atti/blank/magic-atti/sub/RegisterFirst'
+import RegisterSuccess from './src/screens/Atti/blank/magic-atti/sub/RegisterSuccess'
+import RegisterSecond from './src/screens/Atti/blank/magic-atti/sub/RegisterSecond'
+import AlreadyRegistered from './src/screens/Atti/blank/magic-atti/sub/error/AlreadyRegistered'
 
 const App = () => <>
-	<PlaylistEmpty/>
+	<RegisterSuccess/>
 </>
 
 export default App
