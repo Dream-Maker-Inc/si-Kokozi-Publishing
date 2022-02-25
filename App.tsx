5546d565865bb5eb6593e3b51dab4402dec2ab96
Janghyun Han <wkdgus1164@naver.com>
2022-02-25 20:14:43 +0900

    Update - README.MD

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Congratulations from './src/screens/story/record/Congratulations'
+import RecordStart from './src/screens/story/record/RecordStart'
+import RecordCountDown from './src/screens/story/record/RecordCountDown'
+import RecordCheck from './src/screens/story/record/RecordCheck'
+import RecordSave from './src/screens/story/record/RecordSave'
 
 const App = () => <Congratulations/>
 
