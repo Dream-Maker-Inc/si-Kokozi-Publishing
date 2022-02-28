cdaff042531341788d1e703691a58bd4898983bd
Janghyun Han <wkdgus1164@naver.com>
2022-02-28 11:12:46 +0900

    Update - 플레이리스트

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import RecordStart from './src/screens/story/record/RecordStart'
 import RecordCountDown from './src/screens/story/record/RecordCountDown'
 import RecordCheck from './src/screens/story/record/RecordCheck'
 import RecordSave from './src/screens/story/record/RecordSave'
+import BottomTabs from './src/screens/BottomTabs'
 
-const App = () => <Congratulations/>
+const App = () => <BottomTabs/>
 
 export default App
--- src/screens/story/no-blank/playlist/PlaylistMain.tsx
+++ src/screens/story/no-blank/playlist/PlaylistMain.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from '@emotion/native'
-import {ScrollView} from 'react-native'
 import ImagePaths from '../../../../common/ImagePaths'
-import {BeigeSecondColor} from '../../../../common/Colors'
 import AutoHeightImage from 'react-native-auto-height-image'
-import OutlinedAddButton from '../../../../components/global/buttons/outlined/OutlinedAddButton'
+import {BeigeFifthColor, BeigeSecondColor, navyColor, primaryColor} from '../../../../common/Colors'
+import {FlatList} from 'react-native'
+import {PlaylistData} from '../../../../data/Data'
 
 const PageWrapper = styled.SafeAreaView`
   width: 100%;
@@ -12,26 +12,67 @@ const PageWrapper = styled.SafeAreaView`
   background-color: ${BeigeSecondColor};
 `
 
-const StyledScrollView = styled(ScrollView)`
+const HeaderContainer = styled.TouchableOpacity`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
   width: 100%;
+  height: 56px;
+  padding: 0 16px;
 `
 
-const ScreenText = styled(AutoHeightImage)`
-  margin-top: 63px;
-  margin-bottom: 32px;
+const HeaderText = styled.Text`
+  color: ${primaryColor};
+  font-size: 13px;
+  margin-left: 8px;
 `
 
-const ScreenImage = styled(AutoHeightImage)`
-  margin-bottom: 32px;
+const PlaylistItemWrapper = styled.TouchableOpacity`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  padding: 0 16px 16px;
 `
+
+const ItemThumbnail = styled(AutoHeightImage)`
+  border-radius: 8px;
+`
+
+const ItemContents = styled.View`
+  margin-left: 16px;
+`
+
+const ItemTitle = styled.Text`
+  font-size: 15px;
+  color: ${navyColor};
+`
+
+const ItemCaption = styled.Text`
+  font-size: 12px;
+  color: ${BeigeFifthColor};
+  margin-top: 8px;
+`
+
+const RenderPlaylistItem = ({item}) =>
+	<PlaylistItemWrapper>
+		<ItemThumbnail source={ImagePaths.arti.all.thumbnail} width={80}/>
+		<ItemContents>
+			<ItemTitle>{item.name}</ItemTitle>
+			<ItemCaption>{item.date} {item.count}개 {item.runningTime}분</ItemCaption>
+		</ItemContents>
+	</PlaylistItemWrapper>
+
+const renderPlaylist =
+	<FlatList data={PlaylistData} renderItem={RenderPlaylistItem}/>
 
 const PlaylistMain = () =>
 	<PageWrapper>
-		<StyledScrollView contentContainerStyle={{alignItems: 'center'}}>
-			<ScreenText width={320} source={ImagePaths.story.playlist.blank.text}/>
-			<ScreenImage width={200} source={ImagePaths.story.playlist.blank.image}/>
-			<OutlinedAddButton icon="plus" title="플레이리스트 만들기"/>
-		</StyledScrollView>
+		<HeaderContainer>
+			<AutoHeightImage source={ImagePaths.components.icons.plus} width={24}/>
+			<HeaderText>플레이리스트 만들기</HeaderText>
+		</HeaderContainer>
+		{renderPlaylist}
 	</PageWrapper>
 
 export default PlaylistMain
